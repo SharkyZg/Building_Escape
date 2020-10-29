4ea0564c36ca6b72bb7a0d181376e4ffa83b82ab
Marko Sarkanj <marko.sarkanj@gmail.com>
2020-10-29 12:20:20 +0100

    lecture 118. completed

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,43 @@
+// Marko Sarkanj 2020
+
+#include "Grabber.h"
+
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get players viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+	UE_LOG(LogTemp, Warning, TEXT(" Actor viewpoint location %s, rotation: %s"),
+		   *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+
+	// Ray-casting out to certain distance (Reach)
+
+	// See what it hits
+}
